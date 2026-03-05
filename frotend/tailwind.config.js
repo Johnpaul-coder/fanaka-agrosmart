2d364e9469345f93412e66f78e6b210e9826fb63
Jackline <karimijacky123@gmail.com>
2026-03-05 07:55:25 +0300

    tailwind issue resolved

--- frotend/tailwind.config.js
+++ frotend/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/component/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
